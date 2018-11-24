598088beec549209968aea4ff72b0058291cecc1
aburk3 <aburk3@gmail.com>
2018-11-24 10:55:22 -0600

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Application
+
+  @@items = [Item.new("Tomato", 1.50), Item.new("Chips", 8.00)]
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{ |i| i.name == item_name }
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
+end
