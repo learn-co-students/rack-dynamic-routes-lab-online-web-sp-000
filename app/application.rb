e1c9379372ddef15d1a686f0a3bf00320217038a
Diana Badas <diana.badas@gmail.com>
2019-03-18 14:55:42 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      if @@items.include?(item)
+      if @@items.include?(item) #if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else #item.nil?
         resp.write "Item not found"
