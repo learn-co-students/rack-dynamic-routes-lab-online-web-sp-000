588af5d36c9b1c2a35a8c71741a957dd29b7c346
J. Singleton <jennifer.singleton@gmail.com>
2019-03-24 20:19:07 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+require 'pry'
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+      item_name = req.path.split("/items/").last
+      if @@items.find{|item| item_name == item.name}
+        item = @@items.find{|item| item_name == item.name}
+        resp.write "#{item.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+
+  end
+
+end
