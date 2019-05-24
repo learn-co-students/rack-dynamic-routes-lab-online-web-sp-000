36ab5a121494f74470384231ceb78319262f1876
Bry Cox <carl.cc@gmail.com>
2019-05-24 09:24:24 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+
+  @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        resp.write "added #{item}"
+
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
