105d0fe63594aa1d0a55b4e4c0c44a826818cde7
Yingqi Chen <chanwk18@hotmail.com>
2019-06-08 22:46:50 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,34 +3,22 @@ class Application
   #@@items=[]
 
   def call(env)
-    @resp=Rack::Response.new
+    resp=Rack::Response.new
     req=Rack::Request.new(env)
     #binding.pry
     if req.path.match(/items/)
       name=req.path.split("/items/").last
-      @resp.write check_items(name)
-    else
-      @resp.write "Route not found"
-      @resp.status = 404
-    end
-
-    def check_items(item)
-    #  binding.pry
-      if @@items.any?{|t|t.name==item}
-        i= @@items.find{|t|t.name==item}
-        return i.price
+      if i=@@items.find{|t|t.name==name}
+        resp.write i.price
       else
-        @resp.status=400
-        "Item not found"
+        resp.status=400
+        resp.write "Item not found"
       end
+    else
+      resp.write "Route not found"
+      resp.status = 404
     end
-
-
-
-
-    @resp.finish
+    resp.finish
   end
-
-
 
 end
