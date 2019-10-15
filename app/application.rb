51a527b0c3ea675cfa60640228af9c7c717e45fb
Lukie Kang <Lukie Kang>
2019-10-15 23:00:00 +0100

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,19 +4,19 @@ class Application
     def call(env)
         res = Rack::Response.new
         req = Rack::Request.new(env)
-
-        if req.path.match(/items/)
-            item_name = req.path.split("/items").last
-            if @@items.include?(item_name)
-                item = @@items.find{|i| i.name = item_name}
-                res.write("#{item.price}")
-            else
+        if !req.path.match(/items/)
+            res.write("Route not found")
+            res.status = 404
+        else
+            item_name = req.path.split("/items/").last
+            Item.all.each{|i| puts i.name}
+            if !Item.all.find{|i| i.name == item_name}
                 res.write("Item not found")
                 res.status = 400 
+            else 
+                item_search = Item.all.find{|i| i.name = item_name}
+                res.write("#{item_search.price}")
             end
-        else
-            res.write("Path not found")
-            res.status = 404
         end
         res.finish
     end
