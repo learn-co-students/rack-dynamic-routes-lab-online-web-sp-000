7d549e5909b411c2d4238c292bc31d49d355208c
Connor Cowan <ccowan003@comcast.net>
2019-10-17 14:58:53 -0700

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path = "/items"
+      resp.write
+    else
+      resp.write "Request not Found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
