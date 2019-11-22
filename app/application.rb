e9e3de4956138c3a703d333407e262edc9afaa19
Blythe Pollard <blytheepollard@gmail.com>
2019-11-22 00:38:53 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,6 @@ class Application
   if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       if item = @@items.find{|s| s.name == item_name}
-      
         resp.write item.price
       else 
         resp.write "Item not found"
