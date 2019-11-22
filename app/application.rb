21ec6b1f0cffdafee072f93c8270e2f4456b3e97
Blythe Pollard <blytheepollard@gmail.com>
2019-11-22 00:37:25 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,10 +9,10 @@ class Application
   if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       item = @@items.find{|s| s.name == item_name}
-      if item.price
+      if @@items.include?(item)
         resp.write item.price
       else 
-        resp.write "Route not found"
+        resp.write "Item not found"
         resp.status = 400
       end
    else
