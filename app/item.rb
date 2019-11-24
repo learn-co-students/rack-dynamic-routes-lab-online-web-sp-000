7b7c2670579085aebff9da41babc0b0204eb14db
Cody Rickman <cody@creativemindstudios.com>
2019-11-24 16:19:03 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,18 @@
 class Item
   attr_accessor :name, :price
-
+  @@items = []
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
+  end
+
+  def self.findByName(name)
+    @@items.each do |item|
+      if item.name == name
+        return item
+      end
+    end
+    return nil
   end
 end
