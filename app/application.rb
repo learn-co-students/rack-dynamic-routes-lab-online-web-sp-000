ab6e15d5ecdfd9c686b9262cb7da6e8928812ccf
David Cantoran <davidcantoran36@gmail.com>
2020-02-27 01:14:45 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+# require 'pry'
+
+class Application 
+
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split('/').last 
+      found_item = @@items.find {|i| i.name == item_name}
+      
+      if found_item
+        resp.write found_item.price
+      else 
+        resp.write "Item not found"
+        resp.status = 400
+      end 
+    else 
+      resp.status = 404
+      resp.write "Route not found"
+    end 
+    
+    resp.finish
+  end 
+  
+end 
