5d10d25f1a55df9b4ac85e4961aaa760e7f8381d
Charles Sabino <charles.sabino@hotmail.com>
2020-03-16 21:51:44 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_title = req.path.split("/items/").last
+      if
+        @@items.find{|i| i.name == item_title}
+        item = @@items.find{|i| i.name == item_title}
+        resp.write "#{item.price}"
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+    end
+end
