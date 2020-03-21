ea7815f11a6d331ba3a8d7bb8c2d8753612937b6
Ryan Mui <reachryanmui@gmail.com>
2020-03-21 20:28:20 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item = req.path_info.split("/items/").last
-      # binding.pry
-      found_item = @@items.find(item) do |i|
-        binding.pry
+      found_item = @@items.find() do |i|
         i.name  == item
       end
       if found_item != nil
