736a0625f7c3896081c05db20a64675d63812314
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-03 02:27:29 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+class Application
+  
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req =  Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      #above gets last item name from requested url
+      
+      if item = @@items.detect{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+      
+    else
+      resp.status = 404
+      resp.write "Route not found"
+      
+    end
+    resp.finish
+  end
+end
