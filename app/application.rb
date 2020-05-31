6cc1412e8d22e02b549d0bb1121fa16cdd23192a
gabejoseph <gabriel.joseph4@gmail.com>
2020-05-31 15:44:35 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Application
+
+    @@items = []
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last
+            item = @@items.find{|x| x.name == item_name}
+            if item.nil?
+                resp.write "Item not found"
+                resp.status = 400
+            elsif 
+                resp.write item.price
+            end
+
+        else 
+            resp.write "Route not found"
+            resp.status = 404
+        end
+        resp.finish
+    end
+
+
+
+
+end
