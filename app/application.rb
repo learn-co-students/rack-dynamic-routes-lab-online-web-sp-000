495db85f4f977698882a790abc34abaefadc732c
satodd1967 <seth.todd@outlook.com>
2020-06-08 21:23:25 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+require "pry"
+class Application
+ 
+    def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+
+      if req.path.match(/items/)
+
+        item_passed = req.path.split("/items/").last
+        if item = @@items.find {|i| i.name == item_passed}
+            resp.write item.price
+            resp.status = 200
+        else
+            resp.write "Item not found"
+            resp.status = 400
+        end
+        
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+   
+      resp.finish
+    end
+    # binding.pry
+  end
