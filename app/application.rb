6799fa311615124882fab8cbf9f6788763b92894
Snowing222 <snowing222@gmail.com>
2020-07-15 06:41:13 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Application
+    @@items = []
+    
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+        
+        if req.path.match(/items/)
+            item = req.path.split("/items/").last
+        
+            if found =@@items.find{|i|i.name==item}
+                resp.write found.price
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+
+        resp.finish
+
+    end
+
+end
+
