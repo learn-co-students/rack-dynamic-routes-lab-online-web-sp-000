e33531464870cb804683cbc2670fa0bcc672eccf
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-27 02:11:52 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,17 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/"
-      resp.write "something something"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-
+    if req.path.match"/items/"
+      if @@items.include?(@@item)
+        item_price = @@item[1]
+        resp.write write item_price
+      elsif @@items.include?!(@@item)
+        resp.write "Route not found"
+        resp.status = 400
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
     end
 
     resp.finish
