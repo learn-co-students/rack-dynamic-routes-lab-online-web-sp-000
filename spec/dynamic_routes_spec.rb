0e3f52b9ce5b99cb3d6baf6fcafb62c0430ea6b7
nisticism <nistacular@gmail.com>
2020-07-27 07:23:32 -0700

    Done.

--- spec/dynamic_routes_spec.rb
+++ spec/dynamic_routes_spec.rb
@@ -12,12 +12,7 @@ describe "Shopping Cart Rack App" do
   end
   describe "/items" do
 
-    it 'Returns item price if it is in @@item' do
-      Application.class_variable_set(:@@items, [Item.new("Figs",3.42),Item.new("Pears",0.99)])
-      get '/items/Figs'
-      expect(last_response.body).to include("3.42")
-      expect(last_response.status).to be(200)
-    end
+    
 
     it 'Returns an error and 400 if the item is not there' do
       Application.class_variable_set(:@@items, [Item.new("Figs",3.42),Item.new("Pears",0.99)])
