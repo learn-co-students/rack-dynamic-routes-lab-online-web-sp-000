acf3bbfc931d92daca09ce311c0e02b6d04a7bba
William Pannell <lpann5@gmail.com>
2020-11-04 23:00:55 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find do |i|
+        i.name == item_name
+      end
+      if item == nil
+        resp.status = 400
+        resp.write "Item not found"
+      else
+        resp.write item.price
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
+end
