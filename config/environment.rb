f28ce426512ed4c378a2ce1791d736de6b526d3b
lahb2434 <chrishoughton2434@gmail.com>
2021-02-07 10:27:33 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ Bundler.require
 
 require_relative "../app/item.rb"
 require_relative '../app/application'
+# Application.class_variable_set(:@@items, [Item.new("Figs",3.42),Item.new("Pears",0.99)])
