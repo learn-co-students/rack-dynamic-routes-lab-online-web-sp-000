dd7eb2070de6fc60c97fa29da46df3e42b0b3062
McKayla Lankau <lankaukk@yahoo.com>
2021-02-22 01:50:14 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+ 
+    def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+   
+      if req.path=="/items"
+        resp.write "#{item.price}"
+      elsif 
+
+        resp.status = 400
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+   
+      resp.finish
+    end
+  end
