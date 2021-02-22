74c873fb960d42c795ae9a3ed6a52fb5302e174d
McKayla Lankau <lankaukk@yahoo.com>
2021-02-22 01:57:17 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,27 +1,7 @@
-class Application
- 
-    def call(env)
-      resp = Rack::Response.new
-      req = Rack::Request.new(env)
-   
-      if req.path=="/items"
-
-        resp.write "#{item.price}"
-      elsif 
-
-        resp.status = 400
-      else
-        resp.write "Route not found"
-        resp.status = 404
-      end
-   
-      resp.finish
-    end
-  end
-
   class Application
 
     @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+    
     def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
