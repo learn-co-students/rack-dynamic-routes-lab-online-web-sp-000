41ac28a93aa2da2d3b629f40beeb4bfde507d924
Chris Baum <Christopher.Baum@valpo.edu>
2021-03-31 20:34:15 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,23 +2,35 @@ class Application
   @@items = []
 
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    @resp = Rack::Response.new
+    @req = Rack::Request.new(env)
 
     @item_names = @@items.map{|i| "/items/#{i.name}"}
-    if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      if @item_names.include?(req.path)
-        resp.write @@items.find{|i| i.name == item_name}.price
+    if @req.path.match(/items/)
+      @item_name = @req.path.split("/items/").last
+      if @item_names.include?(@req.path)
+        item_price
       else
-        resp.status = 400
-        resp.write "Item not found"
+        error_no_item_found
       end
     else
-      resp.status = 404
-      resp.write "Route not found"
+      error_no_route_found
     end
 
-    resp.finish
+    @resp.finish
+  end
+
+  def item_price
+    @resp.write @@items.find{|i| i.name == @item_name}.price
+  end
+
+  def error_no_route_found
+    @resp.status = 404
+    @resp.write "Route not found"
+  end
+
+  def error_no_item_found
+    @resp.status = 400
+    @resp.write "Item not found"
   end
 end
