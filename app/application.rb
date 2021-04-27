19c5361359b38745cb89713f7520cc1429f15e9f
Hannah M <hannahemalm616@gmail.com>
2021-04-27 20:44:15 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class Application
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",5.23)]
+	def call(env)
+  	resp = Rack::Response.new
+  	req = Rack::Request.new(env)
+  # 	binding.pry
+  	if req.path=="/items/"
+  	  item_name = req.path.split("/items/").last #hand the split method the pieces of the elements to separate 
+  	  if item =  @@items.find{|item| item.name == item_name} 
+  	    resp.write "#{item.price}\n"
+  	   else 
+    	  resp.write "Item not Found"
+        resp.status = 400
+      end 
+    else
+      	resp.write "Route not Found"
+      	resp.status = 404
+  	end
+  end 
+end 
+
