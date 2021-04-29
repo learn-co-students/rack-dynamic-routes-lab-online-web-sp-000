5cbaf7a1fd1ae550c7666ee4d25d638a1c50e9aa
Hannah M <hannahemalm616@gmail.com>
2021-04-29 21:36:53 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,22 +1,23 @@
 require 'pry'
 class Application
-  @@items = [Item.new("Apples",5.23), Item.new("Oranges",5.23)]
+  # @@items = [Item.new("Apples",5.23), Item.new("Oranges",5.23)]
 	def call(env)
   	resp = Rack::Response.new
   	req = Rack::Request.new(env)
   # 	binding.pry
-  	if req.path=="/items/"
-  	  item_name = req.path.split("/items/").last #hand the split method the pieces of the elements to separate 
+  	if req.path.match(/items/)
+  	  item_name = req.path.split("/items/").last #hand the split method the pieces of the elements to separate
   	  if item =  @@items.find{|item| item.name == item_name} 
-  	    resp.write "#{item.price}\n"
+  	    resp.write item.price
   	   else 
-    	  resp.write "Item not Found"
+    	  resp.write "Item not found"
         resp.status = 400
       end 
     else
-      	resp.write "Route not Found"
+      	resp.write "Route not found"
       	resp.status = 404
   	end
+  	resp.finish
   end 
 end 
 
